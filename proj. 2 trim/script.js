5f5d02698d6aa45ede3f7968f6ec15b314668592
DavidOsha <134282397+DavidOsha@users.noreply.github.com>
2024-08-12 09:07:32 -0300

    Add files via upload

---
+++ proj. 2 trim/script.js
@@ -0,0 +1,17 @@
+const caixaPrincipal = document.querySelector(".caixa-principal")
+const caixaPeguntas = document.querySelector(".caixa-peguntas")
+const caixaAlterntivas = document.querySelector(".caixa-alterntivas")
+const caixaResultado = document.querySelector(".caixa-resultado")
+const textoResultado = document.querySelector(".texto-resultado")
+
+const perguntas = [
+    
+    enunciado: "Pergunta 1", 
+    alternativas:[
+        "Alternativa 1",
+        "Alternativa 2"
+
+    ]
+
+    "Pergunta 2"
+]
